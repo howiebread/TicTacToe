39137ea0be57e1978ef882bd6c267e8f789e508b
howiebread <49467548+howiebread@users.noreply.github.com>
2025-05-26 23:32:53 -0400

    Updated the Array to be a 2D array and added the welcome message.

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -3,13 +3,20 @@
 class Program
 {
     // The game board - starts with numbers 1-9 for positions
-    char[] board = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-    const char PLAYER_MARK = 'X';  // Player uses X
+    char[,] board =
+    {
+        { '1', '2', '3' },
+        { '4', '5', '6' },
+        { '7', '8', '9' }
+    };
+
+    const char PLAYER_MARK = 'X'; // Player uses X
     const char COMPUTER_MARK = 'O'; // Computer uses O
-    static Random random = new Random();
-    
+
     static void Main(string[] args)
     {
-        
+        Console.WriteLine("Welcome to Tic Tac Toe!");
+        Console.WriteLine("You are X, Computer is O");
+        Console.WriteLine("Enter numbers 1-9 to make your move\n");
     }
 }
